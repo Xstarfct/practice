197dcf35a00d02859d7b2ec2681ca8f006b7116d
范传涛 <fanchuantao@mistong.com>
2021-06-24 17:07:38 +0800

    回文数字判断

--- dubbo-3.0/src/test/java/com/fct/d/common/NumberTest.java
+++ dubbo-3.0/src/test/java/com/fct/d/common/NumberTest.java
@@ -13,11 +13,39 @@ public class NumberTest extends NoSpringBaseTest {
 
   @Test
   public void testIsPalindrome() {
-
-
+    System.out.println(isPalindrome(1221));
+    System.out.println(isPalindrome(8));
+    System.out.println(isPalindrome(88));
+    System.out.println(isPalindrome(81));
+    System.out.println(isPalindrome(181));
+    System.out.println(isPalindrome(188888881));
   }
 
   public boolean isPalindrome(int x) {
-    return false;
+    if (x < 0) {
+      return false;
+    }
+    if (x < 10) {
+      return true;
+    }
+    int a = 0, b = x;
+    while (x > 0) {
+      a = a * 10 + x % 10;
+      x /= 10;
+    }
+    return a == b;
+  }
+
+  @Test
+  public void test2() {
+    int i = 1221 / 10;
+    System.out.println(i % 10);
+  }
+
+  @Test
+  public void printNumber() {
+    for (int i = 0; i < 500; i++) {
+      new Thread(() -> {}).run();
+    }
   }
 }

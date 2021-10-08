c398599404e845baf34a3d858b0c3886fbf57f9e
haoyu <haoyu@tsign.cn>
2021-10-08 18:32:48 +0800

    FullRoundBalance => 代码逻辑优化

--- rpc/rpc-core/src/main/java/com/fct/rpc/core/balancer/impl/FullRoundBalance.java
+++ rpc/rpc-core/src/main/java/com/fct/rpc/core/balancer/impl/FullRoundBalance.java
@@ -2,24 +2,23 @@ package com.fct.rpc.core.balancer.impl;
 
 import com.fct.rpc.core.balancer.LoadBalance;
 import com.fct.rpc.core.common.ServiceInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 轮询算法
  */
 public class FullRoundBalance implements LoadBalance {
 
-    private int index;
+    private final AtomicInteger count = new AtomicInteger(0);
 
     @Override
-    public synchronized ServiceInfo chooseOne(List<ServiceInfo> services) {
-        // 加锁防止多线程情况下，index超出services.size()
-        if (index >= services.size()) {
-            index = 0;
+    public ServiceInfo chooseOne(List<ServiceInfo> services) {
+        // 防止并发情况下，count超出services.size()
+        if (count.get() >= services.size()) {
+            count.set(0);
         }
-        return services.get(index++);
+        return services.get(count.getAndAdd(1));
     }
 }
